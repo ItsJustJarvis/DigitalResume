d4875ff20ffa144c0722060e4eed216d79f039cd
Reeve Jarvis <reevejarvis@hotmail.com>
2022-07-11 23:30:44 -0700

    Restructure main.js for swup library usage

--- public/js/main.js
+++ public/js/main.js
@@ -1,86 +1,171 @@
+/* eslint-disable no-undef */
 "use strict";
 
 /* eslint-disable no-unused-vars */
 import {initializeApp} from "https://www.gstatic.com/firebasejs/9.8.4/firebase-app.js";
 import {getAnalytics} from "https://www.gstatic.com/firebasejs/9.8.4/firebase-analytics.js";
 
-// Menu items
-const menuBtn = document.querySelector(".menu-btn");
-const hamburger = document.querySelector(".menu-btn__burger");
-const nav = document.querySelector(".nav");
-const menuNav = document.querySelector(".menu-nav");
-const navLinks = document.querySelectorAll(".menu-nav__link");
+const bodyClassPlugin = new SwupBodyClassPlugin({
+    prefix: "page-"
+});
 
-let showMenu = false;
+const scrollPlugin = new SwupScrollPlugin({
+    doScrollingRightAway: true,
+    animateScroll: true,
+    scrollFriction: 0.3,
+    scrollAcceleration: 0.04,
+});
 
-menuBtn.addEventListener("click", toggleMenu);
+// Swup options object
+const options = {
+    containers: ["#swup"],
+    cache: true,
+    plugins: [bodyClassPlugin, scrollPlugin]
+};
 
-function toggleMenu() {
-    if(!showMenu) {
-        hamburger.classList.add("open");
-        nav.classList.add("open");
-        menuNav.classList.add("open");
-        navLinks.forEach(item => item.classList.add("open"));
+const swup = new Swup(options);
 
-        showMenu = true;
-    } else {
-        hamburger.classList.remove("open");
-        nav.classList.remove("open");
-        menuNav.classList.remove("open");
-        navLinks.forEach(item => item.classList.remove("open"));
+init();
 
-        showMenu = false;
+swup.on("contentReplaced", init);
+
+function init(){
+
+    if(document.querySelector(".about")){
+        initializeAboutItems();   
+        initilizeSectionJumps();     
+    }
+    initializeMenu();
+}
+
+function initilizeSectionJumps(){
+    const sectionButtons = document.querySelectorAll(".sectionjump__button");
+    sectionButtons.forEach(button => button.addEventListener("click", jumpTo));
+
+    function jumpTo(event){
+        let button = event.currentTarget;
+        let location = button.innerText.toLowerCase().replace(/[^a-z0-9]/gi, "");
+        let element = document.querySelector(`#${location}`);
+        console.log(element);
+        swup.scrollTo(element);
     }
 }
 
-// Education/Job card content - Collapsible solution based on above and some influence from https://www.w3schools.com/howto/howto_js_collapsible.asp
-const educationButtons = document.querySelectorAll(".education__item__tab");
-const jobItems = document.querySelectorAll(".jobs__item__tab");
+function initializeMenu(){
+    const menuBtn = document.querySelector(".menu-btn");
+    const hamburger = document.querySelector(".menu-btn__burger");
+    const nav = document.querySelector(".nav");
+    const menuNav = document.querySelector(".menu-nav");
+    const navLinks = document.querySelectorAll(".menu-nav__link");
+    
+    let showMenu = false;
 
-educationButtons.forEach(item => {
-    item.addEventListener("click", toggle);
-});
+    menuBtn.addEventListener("click", toggleMenu);
+    navLinks.forEach( link => link.addEventListener("click", toggleMenu));
 
-jobItems.forEach(item => {
-    item.addEventListener("click", toggle);
-});
+    function toggleMenu() {
+        if(!showMenu) {
+            hamburger.classList.add("open");
+            nav.classList.add("open");
+            menuNav.classList.add("open");
+            navLinks.forEach(item => item.classList.add("open"));
 
-function toggle(event){
-    let tab = event.currentTarget;
-    let content = event.currentTarget.nextElementSibling;
+            showMenu = true;
+        } else {
+            hamburger.classList.remove("open");
+            nav.classList.remove("open");
+            menuNav.classList.remove("open");
+            navLinks.forEach(item => item.classList.remove("open"));
 
-    if(!content.classList.contains("open")){
+            showMenu = false;
+        }
+    }
+}
+
+function initializeAboutItems(){
+
+    // About Page Items
+    const educationItems = document.querySelectorAll(".education__item__tab");
+    const jobItems = document.querySelectorAll(".jobs__item__tab");
+    const skillPills = document.querySelectorAll(".pill");
+
+    educationItems.forEach(item => {
+        item.addEventListener("click", toggleEducation);
+    });
+
+    jobItems.forEach(item => {
+        item.addEventListener("click", toggleJob);
+    });
+
+    skillPills.forEach(item => {setPillColor(item);});
+
+    function toggleJob(event){
+        let tab = event.currentTarget;
+        let content = event.currentTarget.nextElementSibling;
+
+        if(tab.classList.contains("open")){
+            tab.classList.remove("open");
+            content.classList.remove("open");
+            return;
+        }
+
+        jobItems.forEach(item => {
+            if(item.classList.contains("open")){
+                item.classList.remove("open");
+            }
+            if(item.nextElementSibling.classList.contains("open")){
+                item.nextElementSibling.classList.remove("open");
+            }       
+        });
+
         tab.classList.add("open");
         content.classList.add("open");
-    } else {
-        tab.classList.remove("open");
-        content.classList.remove("open");
     }
-}
 
-const skillPills = document.querySelectorAll(".pill");
-skillPills.forEach(item => {setPillColor(item);});
+    function toggleEducation(event){
+        let tab = event.currentTarget;
+        let content = event.currentTarget.nextElementSibling;
 
-function setPillColor(item) {
-    switch (item.textContent) {
-    case "0/5":
-        item.style.backgroundColor = "#FF0D0D";
-        break;
-    case "1/5":
-        item.style.backgroundColor = "#FF4E11";
-        break;
-    case "2/5":
-        item.style.backgroundColor = "#FF8E15";
-        break;
-    case "3/5":
-        item.style.backgroundColor = "#FAB733";
-        break;
-    case "4/5":
-        item.style.backgroundColor = "#ACB334";
-        break;
-    case "5/5":
-        item.style.backgroundColor = "#69B34C";
-        break;
+        if(tab.classList.contains("open")){
+            tab.classList.remove("open");
+            content.classList.remove("open");
+            return;
+        }
+
+        educationItems.forEach(item => {
+            if(item.classList.contains("open")){
+                item.classList.remove("open");
+            }
+            if(item.nextElementSibling.classList.contains("open")){
+                item.nextElementSibling.classList.remove("open");
+            }       
+        });
+
+        tab.classList.add("open");
+        content.classList.add("open");
+    }
+
+    function setPillColor(item) {
+        switch (item.textContent) {
+        case "0/5":
+            item.style.backgroundColor = "#FF0D0D";
+            break;
+        case "1/5":
+            item.style.backgroundColor = "#FF4E11";
+            break;
+        case "2/5":
+            item.style.backgroundColor = "#FF8E15";
+            break;
+        case "3/5":
+            item.style.backgroundColor = "#FAB733";
+            break;
+        case "4/5":
+            item.style.backgroundColor = "#ACB334";
+            break;
+        case "5/5":
+            item.style.backgroundColor = "#69B34C";
+            break;
+        }
     }
 }
 
